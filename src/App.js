dc1a50e54670b2632e597e27f2d26fa18d058fc5
kerime abanoz <krmeabnz@gmail.com>
2023-02-18 20:45:46 +0300

    some libraries and stuff

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import About from './components/About';
-import Home from './components/Home';
-import Navbar from './components/Navbar';
-import Projects from './components/Projects';
-import Skills from './components/Skills';
-import Contact from './components/Contact';
+import "./App.css";
+import About from "./components/about/About";
+import Home from "./components/home/Home";
+import Navbar from "./components/navbar/Navbar";
+import Projects from "./components/projects/Projects";
+import Skills from "./components/skills/Skills";
+import Contact from "./components/contact/Contact";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
-      <Projects/>
-      <Skills/>
-      <About/>
-      <Contact/>
+      <Navbar />
+      <Home />
+      <Projects />
+      <Skills />
+      <About />
+      <Contact />
     </div>
   );
 }
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import "./Home.css";
+import profilePic from "../../img/profilepic.jpg";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="home">
+      <div className="hello">
+        <h1 className="title">I'm a <br /> Front-End <br /> Developer <br /> and <br /> UI Designer</h1>
+        <img src={profilePic} alt="profile pic" className="profilePic" />
+      </div>
+      <h1 className="sentence">A MEANİNGFUL SENTENCE ABOUT SOMETHİNG</h1>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
