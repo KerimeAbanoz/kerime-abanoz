4a3ffd8555760f1ac2fec1e97d6cad20b9bd6728
kerime abanoz <krmeabnz@gmail.com>
2023-02-28 15:26:44 +0300

    yani bişeyler

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,43 @@
 import React from "react";
+import Grid from "@mui/material/Grid";
 import "./Home.css";
 import profilePic from "../../img/profilepic.jpg";
+import Box from "@mui/material/Box";
 
 const Home = () => {
   return (
-    <div className="home">
-      <div className="hello">
-        <h1 className="title">I'm a <br /> Front-End <br /> Developer <br /> and <br /> UI Designer</h1>
-        <img src={profilePic} alt="profile pic" className="profilePic" />
-      </div>
-      <h1 className="sentence">A MEANİNGFUL SENTENCE ABOUT SOMETHİNG</h1>
-    </div>
+    <Box sx={{ flexGrow: 1 }}>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <h1 className="title">
+            I'm a <br /> Front-End <br /> Developer <br /> and <br /> UI
+            Designer
+          </h1>
+        </Grid>
+        <Grid item xs={4}>
+          <img src={profilePic} alt="profile pic" className="profilePic" />
+        </Grid>
+        <Grid item xs={12}>
+          <h1 className="sentence">A MEANİNGFUL SENTENCE ABOUT SOMETHİNG</h1>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
 export default Home;
+
+/* <Grid container spacing={2}>
+  <Grid item xs={8}>
+    <Item>xs=8</Item>
+  </Grid>
+  <Grid item xs={4}>
+    <Item>xs=4</Item>
+  </Grid>
+  <Grid item xs={4}>
+    <Item>xs=4</Item>
+  </Grid>
+  <Grid item xs={8}>
+    <Item>xs=8</Item>
+  </Grid>
+</Grid> */
