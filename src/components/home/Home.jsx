c99ac650c3f659d266660b0fd3cbc70b46b4987a
kerime abanoz <krmeabnz@gmail.com>
2023-02-28 18:48:00 +0300

    Home responsive

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -8,14 +8,14 @@ const Home = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       
-      <Grid container spacing={2}>
-        <Grid item xs={8}>
+      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+        <Grid item xs={12} sm={12} md={6}>
           <h1 className="title">
             I'm a <br /> Front-End <br /> Developer <br /> and <br /> UI
             Designer
           </h1>
         </Grid>
-        <Grid item xs={4}>
+        <Grid item xs={12} sm={12} md={6}>
           <img src={profilePic} alt="profile pic" className="profilePic" />
         </Grid>
         <Grid item xs={12}>
