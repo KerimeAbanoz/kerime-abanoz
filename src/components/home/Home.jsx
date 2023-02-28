52529f54418296045916dd3a9ef160841734098f
kerime abanoz <krmeabnz@gmail.com>
2023-02-28 18:50:23 +0300

    ehueheuh

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -7,16 +7,19 @@ import Box from "@mui/material/Box";
 const Home = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
-      
-      <Grid container spacing={{ xs: 2, md: 3 }} columns={{ xs: 4, sm: 8, md: 12 }}>
+      <Grid
+        container
+        spacing={{ xs: 2, md: 3 }}
+        columns={{ xs: 4, sm: 8, md: 12 }}
+      >
+        <Grid item xs={12} sm={12} md={6}>
+          <img src={profilePic} alt="profile pic" className="profilePic" />
+        </Grid>
         <Grid item xs={12} sm={12} md={6}>
           <h1 className="title">
             I'm a <br /> Front-End <br /> Developer <br /> and <br /> UI
             Designer
           </h1>
-        </Grid>
-        <Grid item xs={12} sm={12} md={6}>
-          <img src={profilePic} alt="profile pic" className="profilePic" />
         </Grid>
         <Grid item xs={12}>
           <h1 className="sentence">A MEANİNGFUL SENTENCE ABOUT SOMETHİNG</h1>
