74c6ceeb5cb400415d32aa500ac8596bba820df4
kerime abanoz <krmeabnz@gmail.com>
2023-03-15 09:58:56 +0300

    some tools falan added

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
 
 const Skills = () => {
   return (
-    <div>Skills</div>
-  )
-}
+    <div style={{ display: "flex", flextWrap: "wrap" }}>
+      <div style={{ flex: "50%" }}>
+        <h1>Languages and Libraries</h1>
+        <ul>
+          <li>HTML5</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>SQL</li>
+          <li>Python</li>
+          <li>React.js</li>
+          <li>Vue.js</li>
+        </ul>
+      </div>
+      <div style={{ flex: "50%" }}>
+        <h1>Tools</h1>
+      </div>
+      <div style={{ flex: "50%" }}>Others</div>
+    </div>
+  );
+};
 
-export default Skills
+export default Skills;
